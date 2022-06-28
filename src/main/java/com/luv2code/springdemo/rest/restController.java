859b064e89399805ab1d305b002c6ca669f80271
zain ahmed <zainahmed01997@gmail.com>
2022-06-29 04:43:56 +0530

    added author object injection for new books left empty space for 2 exception senarios

--- src/main/java/com/luv2code/springdemo/rest/restController.java
+++ src/main/java/com/luv2code/springdemo/rest/restController.java
@@ -29,13 +29,16 @@ public class restController {
 	
 	@PostConstruct
 	public void loadData() {
+		
 		theBooks   = new ArrayList<Book>();
 		theAuthors = new ArrayList<Author>();
 		
 		Author a1 = new Author(1,"zain ahmed",13);
 		Book b1 = new Book("1","corejava","1200","book about java",a1);
+		
 		Author a2 = new Author(2,"ayesha ahmed",10);
 		Book b2 = new Book("2","advancedjava","120","book about advanced java",a2);
+		
 		Author a3 = new Author(3,"farhan ahmed",33);
 		Book b3 = new Book("3","html","200","book about html java",a3);
 
@@ -75,6 +78,50 @@ public class restController {
 		return theAuthors.get(authorId);
 	}
 
+	// post book current 
+	@PostMapping("/books")
+	public Book addStudent(@RequestBody BookAuthor theBookAuthors) {  
+		Author author = null;
+		boolean bookExists   = false;
+		boolean authorExists = false;
+		for(int i = 0 ; i < theBooks.size() ; i++) {
+			if(theBooks.get(i).getBookName().equals(theBookAuthors.getBookName())) {
+				bookExists = true;
+				System.out.println(theBooks.get(i).getBookName()+" = "+theBookAuthors.getBookName());
+				if(theBookAuthors.getAuthorName().equals(theBooks.get(i).getBookAuthor().getAuthorName())) {
+					System.out.println(theBookAuthors.getAuthorName()+" = "+theBooks.get(i).getBookAuthor().getAuthorName());
+					author = theBooks.get(i).getBookAuthor();
+					authorExists = true;
+					break;
+				}
+			}
+		}
+		if(bookExists == false) {
+		for(int i = 0 ; i < theAuthors.size() ; i++) {
+			if(theBookAuthors.getAuthorName().equals(theAuthors.get(i).getAuthorName())){
+				authorExists = true;
+				author = theAuthors.get(i);
+				break;
+			}
+		}
+		}
+		System.out.println("bookExists: "+ bookExists + "  authorExists :"+ authorExists);
+		if(bookExists && authorExists) {
+			// throw an exception here if book with the same author exists...
+			theBookAuthors.setAuthorName("already exists");
+			theBookAuthors.setBookName("alreay exists");
+			return null;
+		}
+		else if(bookExists == false && authorExists == false) {
+			// throw a author not found exception
+			return null;
+		}
+		else {
+			// save the book before returning 
+			Book result = new Book(theBookAuthors.getBookId(),theBookAuthors.getBookName(),theBookAuthors.getBookPrice(),theBookAuthors.getJournal(),author);
+			return result;
+		}
+	}
 	
 	// add mapping for POST /book  -add new book
 //		@PostMapping("/books")
@@ -88,49 +135,49 @@ public class restController {
 	
 	//----------------testing exception upon adding a book--------------------
 
-	@PostMapping("/books")
-	public Book addBooks(@RequestBody BookAuthor theBookAuthors) {  
-		boolean bookExists = false;
-		boolean authorExists = false;
-		int temp = 0;
-		for(int i = 0 ; i < theBooks.size(); i++) {
-			if(theBookAuthors.getBookName().equals(theBooks.get(i).getBookName())) {
-			bookExists = true;	
-			if(theBookAuthors.getAuthorName().equals(theBooks.get(i).getBookAuthor())) {
-				authorExists = true;
-				break;
-			}
-		}
-		
-		}
-		if(bookExists && authorExists) {
-			// ---------------throw an exception here ----------------------
-			throw new BookAlreadyExistsException("Book already exists - "+theBookAuthors.getBookName()+" by "+theBookAuthors.getAuthorName());
-		}else {
-			for(int i = 0 ; i < theAuthors.size() ; i++) {
-				if(theBookAuthors.getAuthorName().equals(theAuthors.get(i).getAuthorName())) {
-					temp  = i;
-					break;
-				}
-			}
-			Author a = theAuthors.get(temp);
-			Book b = new Book(theBookAuthors.getBookId(), theBookAuthors.getBookName(), theBookAuthors.getBookPrice(), theBookAuthors.getJournal(), a);
-			theBooks.add(b);
-			return b;
-		}
-	}
-	
-	@ExceptionHandler
-	public ResponseEntity<BookErrorResponse> handleException(BookAlreadyExistsException exc){
-		//create a StudentErrorResponse
-		BookErrorResponse error = new BookErrorResponse();
-		error.setStatus(HttpStatus.NOT_FOUND.value());
-		error.setMessage(exc.getMessage());
-		error.setTimeStamp(System.currentTimeMillis());
-		//return ResponseEntity
-		
-		return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
-	}
+//	@PostMapping("/books")
+//	public Book addBooks(@RequestBody BookAuthor theBookAuthors) {  
+//		boolean bookExists = false;
+//		boolean authorExists = false;
+//		int temp = 0;
+//		for(int i = 0 ; i < theBooks.size(); i++) {
+//			if(theBookAuthors.getBookName().equals(theBooks.get(i).getBookName())) {
+//			bookExists = true;	
+//			if(theBookAuthors.getAuthorName().equals(theBooks.get(i).getBookAuthor())) {
+//				authorExists = true;
+//				break;
+//			}
+//		}
+//		
+//		}
+//		if(bookExists && authorExists) {
+//			// ---------------throw an exception here ----------------------
+//			throw new BookAlreadyExistsException("Book already exists - "+theBookAuthors.getBookName()+" by "+theBookAuthors.getAuthorName());
+//		}else {
+//			for(int i = 0 ; i < theAuthors.size() ; i++) {
+//				if(theBookAuthors.getAuthorName().equals(theAuthors.get(i).getAuthorName())) {
+//					temp  = i;
+//					break;
+//				}
+//			}
+//			Author a = theAuthors.get(temp);
+//			Book b = new Book(theBookAuthors.getBookId(), theBookAuthors.getBookName(), theBookAuthors.getBookPrice(), theBookAuthors.getJournal(), a);
+//			theBooks.add(b);
+//			return b;
+//		}
+//	}
+//	
+//	@ExceptionHandler
+//	public ResponseEntity<BookErrorResponse> handleException(BookAlreadyExistsException exc){
+//		//create a StudentErrorResponse
+//		BookErrorResponse error = new BookErrorResponse();
+//		error.setStatus(HttpStatus.NOT_FOUND.value());
+//		error.setMessage(exc.getMessage());
+//		error.setTimeStamp(System.currentTimeMillis());
+//		//return ResponseEntity
+//		
+//		return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
+//	}
 	//----------------testing verison--------------------
 	
 	// add mapping for POST /author  -add new author
